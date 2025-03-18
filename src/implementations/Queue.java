4c7294cdf95e80778e6b880ed49bfcd07375b5cc
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-03-17 19:12:49 -0600

    Update Queue.java

--- src/implementations/Queue.java
+++ src/implementations/Queue.java
@@ -1,9 +1,42 @@
 package implementations;
+import java.util.NoSuchElementException;
 
-public class Queue {
+import utilities.QueueADT;
+
+public class Queue<E> implements QueueADT<E> {
 
 	public Queue() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void enqueue(E item) throws NullPointerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E dequeue() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
