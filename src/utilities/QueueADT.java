a16e97b937abc985988ab336314956472cb71925
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-03-17 19:06:25 -0600

    Update QueueADT.java

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,9 +1,40 @@
-package utilities;
+import java.util.NoSuchElementException;
 
-public class QueueADT {
+/**
+ * A queue ADT (First-In-First-Out).
+ * @param <E> The type of elements stored in the queue.
+ */
+public interface QueueADT<E> {
+    /**
+     * Add an item to the rear of the queue.
+     * @param item The item to add.
+     * @throws NullPointerException if the item is null.
+     */
+    void enqueue(E item) throws NullPointerException;
 
-	public QueueADT() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Remove and return the front item of the queue.
+     * @return The front item.
+     * @throws NoSuchElementException if the queue is empty.
+     */
+    E dequeue() throws NoSuchElementException;
 
+    /**
+     * Return the front item without removing it.
+     * @return The front item.
+     * @throws NoSuchElementException if the queue is empty.
+     */
+    E peek() throws NoSuchElementException;
+
+    /**
+     * Check if the queue is empty.
+     * @return true if empty, false otherwise.
+     */
+    boolean isEmpty();
+
+    /**
+     * Get the number of items in the queue.
+     * @return The size.
+     */
+    int size();
 }
