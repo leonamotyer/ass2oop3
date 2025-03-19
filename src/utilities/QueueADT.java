3cf529a1dc99e4d176e593a672d686d5548d616a
OOP Project <davontemc.suf@gmail.com>
2025-03-19 13:07:09 -0600

    Added pre and post conditions

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -3,38 +3,74 @@ import java.util.NoSuchElementException;
 /**
  * A queue ADT (First-In-First-Out).
  * @param <E> The type of elements stored in the queue.
+ * 
+ * Class Definition: This interface represents the public contract for the
+ * implementation of Queue for Assignment 2. 
  */
 public interface QueueADT<E> {
     /**
-     * Add an item to the rear of the queue.
+     * Method to add an item to the rear of the queue.
+     * 
+     * Precondition: A valid Queue object exists and has items in 
+     * the Queue.
+	 * 
+	 * Postcondition: An <E> item specified
+	 * in the argument is added to the end of the Queue.
+     * 
      * @param item The item to add.
      * @throws NullPointerException if the item is null.
      */
     void enqueue(E item) throws NullPointerException;
 
     /**
-     * Remove and return the front item of the queue.
+     * Method to remove and return the front item of the queue.
+     * 
+     * Precondition: A valid Queue object exists and has items in 
+     * the Queue.
+	 * 
+	 * Postcondition: A <E> item representing the current 
+	 * first item in the Queue that was removed.
+     * 
      * @return The front item.
      * @throws NoSuchElementException if the queue is empty.
      */
     E dequeue() throws NoSuchElementException;
 
     /**
-     * Return the front item without removing it.
+     * Returns the front item without removing it.
+     * 
+     * Precondition: A valid Queue object exists and has items in 
+     * the Queue.
+	 * 
+	 * Postcondition: A <E> item representing the current 
+	 * top item in the Queue is returned.
+     * 
      * @return The front item.
      * @throws NoSuchElementException if the queue is empty.
      */
     E peek() throws NoSuchElementException;
 
     /**
-     * Check if the queue is empty.
+     * Method to check if the queue is empty.
+     * 
+     * Precondition: A valid Queue object exists.
+	 * 
+	 * Postcondition: A boolean value representing if there are 
+	 * contents in the Queue.
+     * 
      * @return true if empty, false otherwise.
      */
     boolean isEmpty();
 
     /**
      * Get the number of items in the queue.
+     * 
+     * Precondition: A valid Queue object exists.
+	 * 
+	 * Postcondition: The size of the Queue object is returned.
+     * 
      * @return The size.
      */
     int size();
+   
 }
--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -3,37 +3,71 @@ import java.util.NoSuchElementException;
 /**
  * A stack ADT (Last-In-First-Out).
  * @param <E> The type of elements stored in the stack.
+ * 
+ * Class Definition: This interface represents the public contract for the
+ * implementation of Stack for Assignment 2. 
  */
 public interface StackADT<E> {
     /**
-     * Add an item to the top of the stack.
+     * Method to add an item to the top of the stack.
+     * 
+     * Precondition: A valid Stack object exists.
+	 * 
+	 * Postcondition: An <E> item specified
+	 * in the argument is added to the stack.
+	 * 
      * @param item The item to add.
      * @throws NullPointerException if the item is null.
      */
     void push(E item) throws NullPointerException;
 
     /**
-     * Remove and return the top item of the stack.
+     * Method to remove and return the top item of the stack.
+     * 
+     * Precondition: A valid Stack object exists and has items in 
+     * the Stack.
+	 * 
+	 * Postcondition: A <E> item representing the current 
+	 * top item in the Stack that was removed.
+     * 
      * @return The top item.
      * @throws NoSuchElementException if the stack is empty.
      */
     E pop() throws NoSuchElementException;
 
     /**
-     * Return the top item without removing it.
+     * Method to return the top item without removing it.
+     * 
+     * Precondition: A valid Stack object exists and has items in 
+     * the Stack.
+	 * 
+	 * Postcondition: A <E> item representing the current 
+	 * top item in the Stack is returned.
+     * 
      * @return The top item.
      * @throws NoSuchElementException if the stack is empty.
      */
     E peek() throws NoSuchElementException;
 
     /**
-     * Check if the stack is empty.
+     * Method to check if the stack is empty.
+     * 
+     * Precondition: A valid Stack object exists.
+	 * 
+	 * Postcondition: A boolean value representing if there are 
+	 * contents in the Stack.
+     * 
      * @return true if empty, false otherwise.
      */
     boolean isEmpty();
 
     /**
-     * Get the number of items in the stack.
+     * Method to get the number of items in the stack.
+     * 
+     * Precondition: A valid Stack object exists.
+	 * 
+	 * Postcondition: The size of the Stack object is returned.
+     * 
      * @return The size.
      */
     int size();
